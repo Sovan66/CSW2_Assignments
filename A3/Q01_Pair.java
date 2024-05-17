1e45b983dc73cea5636665736906e9ca0b71cb2c
Sovan66 <147400924+Sovan66@users.noreply.github.com>
2024-05-17 18:11:16 +0530

    Add files via upload

---
+++ A3/Q01_Pair.java
@@ -0,0 +1,34 @@
+
+package genericsCollection;
+
+public class Q01_Pair<K, V> {
+	private K key;
+	private V value;
+
+	public Q01_Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public static void main(String[] args) {
+		Q01_Pair<String, Integer> pair = new Q01_Pair<>("Example", 42);
+		System.out.println("Key: " + pair.getKey() + ", Value: " + pair.getValue());
+	}
+
+}
---
+++ A3/Q12_PriorityQueue.java
@@ -0,0 +1,22 @@
+
+package genericsCollection;
+
+import java.util.PriorityQueue;
+
+public class Q12_PriorityQueue {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 10, 8, 7, 3, 4, 6, 5, 9 };
+
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+		for (int num : arr) {
+			minHeap.offer(num);
+		}
+
+		System.out.println("Dequeued elements of the min heap:");
+		while (!minHeap.isEmpty()) {
+			int minElement = minHeap.poll();
+			System.out.println(minElement);
+		}
+	}
+}
